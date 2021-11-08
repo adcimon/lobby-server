a1013ed33efc1a77a05a1c6eae3bdcca9c38ce20
adcimon <Adri.Ciborro.Montes@gmail.com>
2021-11-08 11:10:35 +0100

    Minor change

--- src/validation/validation.schema.ts
+++ src/validation/validation.schema.ts
@@ -23,7 +23,8 @@ export const CreateRoomSchema = yup.object().shape(
     token: yup.string().required().typeError('Invalid token'),
     uuid: yup.string().required().typeError('Invalid uuid'),
     name: yup.string().required().typeError('Invalid name'),
-    password: yup.string().typeError('Invalid password')
+    password: yup.string().typeError('Invalid password'),
+    icon: yup.string().typeError('Invalid icon')
 });
 
 export const JoinRoomSchema = yup.object().shape(
