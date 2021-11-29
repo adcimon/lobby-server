208b9a0f0b220e4291534121d0ed50e6165137fa
adcimon <Adri.Ciborro.Montes@gmail.com>
2021-11-29 17:38:05 +0100

    Added name and password validation

--- src/validation/validation.schema.ts
+++ src/validation/validation.schema.ts
@@ -1,55 +1,49 @@
 import * as yup from 'yup';
 
-/**
- * Password Regular Expression
- *
- * (?=.*\d)             Should contain at least 1 digit.
- * (?=.*[a-z])          Should contain at least 1 lower case.
- * (?=.*[A-Z])          Should contain at least 1 upper case.
- * [a-zA-Z0-9]{8,20}    Should contain from 8 to 20 from the previous characters.
- * 
- */
-const PASSWORD_REGEXP = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,20}$/;
-const PASSWORD_MESSAGE = "Password must contain from 8 to 20 characters, 1 lowercase, 1 uppercase and 1 number";
+const ROOMNAME_REGEXP = /^[a-zA-Z]+(.){4,20}$/;
+const ROOMNAME_MESSAGE = "Room name must start with an alpha character and contain from 5 to 20 characters";
+
+const PASSWORD_REGEXP = /^[a-zA-Z]+(.){7,20}$/;
+const PASSWORD_MESSAGE = "Password must start with an alpha character and contain from 8 to 20 characters";
 
 export const PingSchema = yup.object().shape(
 {
-    token:      yup.string().required('Token is required').typeError('Invalid token'),
-    uuid:       yup.string().required('UUID is required').typeError('Invalid UUID')
+    token:      yup.string().required('Token is required'),
+    uuid:       yup.string().required('UUID is required')
 });
 
 export const GetRoomSchema = yup.object().shape(
 {
-    token:      yup.string().required('Token is required').typeError('Invalid token'),
-    uuid:       yup.string().required('UUID is required').typeError('Invalid UUID')
+    token:      yup.string().required('Token is required'),
+    uuid:       yup.string().required('UUID is required')
 });
 
 export const GetRoomsSchema = yup.object().shape(
 {
-    token:      yup.string().required('Token is required').typeError('Invalid token'),
-    uuid:       yup.string().required('UUID is required').typeError('Invalid UUID')
+    token:      yup.string().required('Token is required'),
+    uuid:       yup.string().required('UUID is required')
 });
 
 export const CreateRoomSchema = yup.object().shape(
 {
-    token:      yup.string().required('Token is required').typeError('Invalid token'),
-    uuid:       yup.string().required('UUID is required').typeError('Invalid UUID'),
-    name:       yup.string().required('Name is required').typeError('Invalid name'),
-    password:   yup.string().matches(PASSWORD_REGEXP, { message: PASSWORD_MESSAGE, excludeEmptyString: true }).typeError('Invalid password'),
-    hidden:     yup.boolean().typeError('Invalid hidden'),
-    icon:       yup.string().typeError('Invalid icon')
+    token:      yup.string().required('Token is required'),
+    uuid:       yup.string().required('UUID is required'),
+    name:       yup.string().required('Name is required').matches(ROOMNAME_REGEXP, ROOMNAME_MESSAGE),
+    password:   yup.string().matches(PASSWORD_REGEXP, { message: PASSWORD_MESSAGE, excludeEmptyString: true }),
+    hidden:     yup.boolean(),
+    icon:       yup.string().url().max(200)
 });
 
 export const JoinRoomSchema = yup.object().shape(
 {
-    token:      yup.string().required('Token is required').typeError('Invalid token'),
-    uuid:       yup.string().required('UUID is required').typeError('Invalid UUID'),
-    name:       yup.string().required('Name is required').typeError('Invalid name'),
-    password:   yup.string().matches(PASSWORD_REGEXP, { message: PASSWORD_MESSAGE, excludeEmptyString: true }).typeError('Invalid password')
+    token:      yup.string().required('Token is required'),
+    uuid:       yup.string().required('UUID is required'),
+    name:       yup.string().required('Name is required').matches(ROOMNAME_REGEXP, ROOMNAME_MESSAGE),
+    password:   yup.string().matches(PASSWORD_REGEXP, { message: PASSWORD_MESSAGE, excludeEmptyString: true })
 });
 
 export const LeaveRoomSchema = yup.object().shape(
 {
-    token:      yup.string().required('Token is required').typeError('Invalid token'),
-    uuid:       yup.string().required('UUID is required').typeError('Invalid UUID')
+    token:      yup.string().required('Token is required'),
+    uuid:       yup.string().required('UUID is required')
 });
