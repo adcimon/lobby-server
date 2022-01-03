5692c697f2db31f5fa77c9df7623627c931bf937
adcimon <37084114+adcimon@users.noreply.github.com>
2022-01-03 16:49:00 +0100

    Minor changes

--- src/lobby/lobby.module.ts
+++ src/lobby/lobby.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { SessionModule } from '../session/session.module';
 import { UserModule } from '../user/user.module';
 import { RoomModule } from '../room/room.module';
-import { SessionModule } from '../session/session.module';
 import { LobbyGateway } from './lobby.gateway';
 import { NotificationService } from './notification.service';
 
 @Module({
-    imports: [AuthModule, UserModule, RoomModule, SessionModule],
+    imports: [AuthModule, SessionModule, UserModule, RoomModule],
     controllers: [],
     providers: [LobbyGateway, NotificationService],
     exports: []
