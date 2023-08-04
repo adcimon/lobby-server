d6b282778072b8b928184562ea3768f87e6cb5d8
adcimon <37084114+adcimon@users.noreply.github.com>
2023-08-04 12:52:26 +0200

    Updated validation schema

--- src/validation/validation.schema.ts
+++ src/validation/validation.schema.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
-const ROOMNAME_REGEXP = /^[a-zA-Z]+(.){4,20}$/;
-const ROOMNAME_MESSAGE = 'Room name must start with an alpha character and contain from 5 to 20 characters';
+const ROOM_NAME_REGEXP = /^[a-zA-Z]+(.){4,20}$/;
+const ROOM_NAME_MESSAGE = 'Room name must start with an alpha character and contain from 5 to 20 characters';
 
 const PASSWORD_REGEXP = /^[a-zA-Z]+(.){7,20}$/;
 const PASSWORD_MESSAGE = 'Password must start with an alpha character and contain from 8 to 20 characters';
@@ -10,53 +10,56 @@ function emptyStringToNull(value, originalValue) {
 	return originalValue === '' ? null : value;
 }
 
+const UuidSchema = yup.string();
+const TokenSchema = yup.string();
+const NameSchema = yup.string().matches(ROOM_NAME_REGEXP, ROOM_NAME_MESSAGE);
+const PasswordSchema = yup.string().matches(PASSWORD_REGEXP, { message: PASSWORD_MESSAGE, excludeEmptyString: true });
+const HiddenSchema = yup.boolean();
+const SizeSchema = yup.number().integer().positive().min(1).nullable(true).transform(emptyStringToNull);
+const IconSchema = yup.string().url().max(500);
+const TargetSchema = yup.string();
+
 const PingSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
+	uuid: UuidSchema.required('UUID is required'),
+	token: TokenSchema.required('Token is required'),
 });
 
 const GetRoomSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
+	uuid: UuidSchema.required('UUID is required'),
+	token: TokenSchema.required('Token is required'),
 });
 
 const GetRoomsSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
+	uuid: UuidSchema.required('UUID is required'),
+	token: TokenSchema.required('Token is required'),
 });
 
 const CreateRoomSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
-	name: yup.string().required('Name is required').matches(ROOMNAME_REGEXP, ROOMNAME_MESSAGE),
-	password: yup.string().matches(PASSWORD_REGEXP, { message: PASSWORD_MESSAGE, excludeEmptyString: true }),
-	hidden: yup.boolean(),
-	size: yup.number().integer().positive().min(1).nullable(true).transform(emptyStringToNull),
-	icon: yup.string().url().max(500),
+	uuid: UuidSchema.required('UUID is required'),
+	token: TokenSchema.required('Token is required'),
+	name: NameSchema.required('Name is required'),
+	password: PasswordSchema,
+	hidden: HiddenSchema,
+	size: SizeSchema,
+	icon: IconSchema,
 });
 
 const JoinRoomSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
-	name: yup.string().required('Name is required').matches(ROOMNAME_REGEXP, ROOMNAME_MESSAGE),
-	password: yup.string().matches(PASSWORD_REGEXP, { message: PASSWORD_MESSAGE, excludeEmptyString: true }),
+	uuid: UuidSchema.required('UUID is required'),
+	token: TokenSchema.required('Token is required'),
+	name: NameSchema.required('Name is required'),
+	password: PasswordSchema,
 });
 
 const LeaveRoomSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
+	uuid: UuidSchema.required('UUID is required'),
+	token: TokenSchema.required('Token is required'),
 });
 
 const KickUserSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
-	target: yup.string().required('Target is required'),
-});
-
-const SendTextSchema = yup.object().shape({
-	uuid: yup.string().required('UUID is required'),
-	token: yup.string().required('Token is required'),
-	text: yup.string().required('Text is required'),
+	uuid: UuidSchema.required('UUID is required'),
+	token: TokenSchema.required('Token is required'),
+	target: TargetSchema.required('Target is required'),
 });
 
 export const ValidationSchema = {
@@ -67,5 +70,4 @@ export const ValidationSchema = {
 	JoinRoomSchema,
 	LeaveRoomSchema,
 	KickUserSchema,
-	SendTextSchema,
 };
